9e80c4110c48f0da902bd3ff893667b02df84a19
janepoligyn <ea.polig@gmail.com>
2025-07-24 21:27:25 -0500

    encapsulation recap, inheritance

---
+++ src/day35_inheritance/person/InPersonStudent.java
@@ -0,0 +1,7 @@
+package day35_inheritance.person;
+
+public class InPersonStudent extends Student{
+    public void schoolBus () {
+        System.out.println(name + " is taking school bus");
+    }
+}

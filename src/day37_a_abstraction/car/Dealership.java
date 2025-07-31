248a3bd59b06a43553f3af9deddfe498a0a0893f
janepoligyn <ea.polig@gmail.com>
2025-07-30 21:06:00 -0500

    encapsulation recap, inheritance

---
+++ src/day37_a_abstraction/car/Dealership.java
@@ -0,0 +1,27 @@
+package day37_a_abstraction.car;
+
+import day34_c_inheritance.animal.Cat;
+
+public class Dealership {
+    public static void main(String[] args) {
+        //Car car = new Car();
+        //car.start();
+        // Abstract Class cannot have object created from it
+
+        Toyota toyota = new Toyota();
+        toyota.start();
+
+        Honda honda = new Honda();
+        honda.start();
+
+        Tesla tesla = new Tesla();
+        tesla.start();
+        tesla.charge();
+
+
+        // Abstract Class cannot have object created from it
+        //ElectricCar eC = new ElectricCar () {
+        //eC.charge();
+
+    }
+}

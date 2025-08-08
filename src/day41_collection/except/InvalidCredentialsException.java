9284b411e8af39d0790844fff054412fbe76d0c2
janepoligyn <ea.polig@gmail.com>
2025-08-07 21:14:30 -0500

    collection

---
+++ src/day41_collection/except/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package day41_collection.except;
+
+public class InvalidCredentialsException extends Exception{
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
+
